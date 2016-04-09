b0e59b4efac836e8b254f300fba64646cb825ad3
Arto Rantanen <arto.rantanen@helsinki.fi>
2016-04-09 11:11:47 +0300

    Spring

--- src/main/java/ohtu/io/ConsoleIO.java
+++ src/main/java/ohtu/io/ConsoleIO.java
@@ -1,8 +1,10 @@
 
 package ohtu.io;
 
-import java.util.Scanner;
+import org.springframework.stereotype.Component;
 
+import java.util.Scanner;
+@Component
 public class ConsoleIO implements IO {
     private Scanner scanner = new Scanner(System.in);
     
